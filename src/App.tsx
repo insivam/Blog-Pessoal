5bc533264635b0ac86223856ed7ef044fa967ff8
insivam <88207216+insivam@users.noreply.github.com>
2022-05-25 17:50:15 -0300

    Add MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import './App.css';
-import Home from './page/home/Home';
+import { Grid } from "@material-ui/core";
+import React from "react";
+import "./App.css";
+import Home from "./pages/home/Home";
+import Navbar from "./components/static/navbar/Navbar";
+import Footer from "./components/static/footer/Footer";
 
 function App() {
   return (
-    <Home />
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
   );
 }
 
